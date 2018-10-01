f36f4b4faca4db6aac4c716d556f44fc05cf4184
yurineko <beerneko@yahoo.com>
2018-10-01 15:46:58 -0500

    changed css ckeditor,before switching pc

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -9,12 +9,10 @@
             {{ Form::hidden('_method','PUT') }}
             {{ Form::submit('Submit',['class'=>'btn btn-primary']) }}
             <div class="form-group">
-                    {{Form::label('title','Title', ['class' => 'label-post']) }}
-                    {{Form::text('title',$post->title,['class'=>'form-control','placeholder'=>'Title'])}}
+                    {{Form::text('title',$post->title,['class'=>'form-control','placeholder'=>'Title','maxlength' => 49])}}
             </div>
-            {{-- <span class="label label-success" style="cursor: pointer;padding:1px;" onclick="$('#cke_1_top').hide('fast');">Editer Show/Hide</span> --}}
             <div class="form-group">
-                {{Form::textarea('body',$post->body,['id'=>'article-ckeditor','class'=>'form-control','placeholder'=>'Title'])}}
+                {{Form::textarea('body',$post->body,['id'=>'test','class'=>'form-control','placeholder'=>'Title'])}}
             </div>
             <div class="form-group">
                     {{ Form::file('cover_image')}}
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -9,7 +9,6 @@
                     @if(!Auth::guest())
                         @if(Auth::user()->id == $post->user_id)
                         <a href="/posts/{{ $post->id }}/edit" class="btn btn-default btn-sm">Edit</a>
-
                             {!! Form::open(['action' => ['PostsController@destroy', $post->id], 'method'=>'POST', 'class'=>'pull-right']) !!}
                                 {{ Form::hidden('_method','DELETE') }}
                                 {{ Form::submit('Delete',['class'=>'btn btn-default btn-sm']) }}
