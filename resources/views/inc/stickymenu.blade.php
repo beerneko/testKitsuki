9ef7781eb594a1da58b1ab2ec45d60f254da052b
Beer Neko <beerneko@yahoo.com>
2018-10-28 18:06:35 -0500

    Added stickymenu

---
+++ resources/views/inc/stickymenu.blade.php
@@ -0,0 +1,7 @@
+<nav class="floating-menu">
+    <a href="#sakura">&#9658;&nbsp;&nbsp;Kitsuki <span class="floating-animate">C</span>astle</a>
+    <a href="#tomaya">&#9658;&nbsp;&nbsp;<span class="floating-animate">D</span>own<span class="floating-animate">T</span>own Kitsuki</a>
+    <a href="#gyoko">&#9658;&nbsp;&nbsp;Kitsuki Fisherman's <span class="floating-animate">B</span>ay</a>
+    <a href="#wasyoku">&#9658;&nbsp;&nbsp;Kitsuki <span class="floating-animate">G</span>ourmet</a>
+    <a href="#onsen">&#9658;&nbsp;&nbsp;Kitsuki <span class="floating-animate">O</span>n<span class="floating-animate">s</span>en</a>
+</nav>
