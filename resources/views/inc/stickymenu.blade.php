e7d18017e9317a78410bb1bd389ae2bb687722c4
Beer Neko <beerneko@yahoo.com>
2018-10-29 11:49:02 -0500

    fixed css for fixed menu block

--- resources/views/inc/stickymenu.blade.php
+++ resources/views/inc/stickymenu.blade.php
@@ -1,7 +1,19 @@
 <nav class="floating-menu">
-    <a href="#sakura">&#9658;&nbsp;&nbsp;Kitsuki <span class="floating-animate">C</span>astle</a>
-    <a href="#tomaya">&#9658;&nbsp;&nbsp;<span class="floating-animate">D</span>own<span class="floating-animate">T</span>own Kitsuki</a>
-    <a href="#gyoko">&#9658;&nbsp;&nbsp;Kitsuki Fisherman's <span class="floating-animate">B</span>ay</a>
-    <a href="#wasyoku">&#9658;&nbsp;&nbsp;Kitsuki <span class="floating-animate">G</span>ourmet</a>
-    <a href="#onsen">&#9658;&nbsp;&nbsp;Kitsuki <span class="floating-animate">O</span>n<span class="floating-animate">s</span>en</a>
+    <div class="row">
+        <div class="col-sm-2">
+            <a href="#sakura">&#9658;&nbsp;&nbsp;Kitsuki <span class="floating-animate">C</span>astle</a>
+        </div>     
+        <div class="col-sm-3">
+            <a href="#tomaya">&#9658;&nbsp;&nbsp;<span class="floating-animate">D</span>own<span class="floating-animate">T</span>own Kitsuki</a>
+        </div>     
+        <div class="col-sm-2">
+            <a href="#gyoko">&#9658;&nbsp;&nbsp;Kitsuki Fisherman's <span class="floating-animate">B</span>ay</a>
+        </div>     
+        <div class="col-sm-2">
+            <a href="#wasyoku">&#9658;&nbsp;&nbsp;Kitsuki <span class="floating-animate">G</span>ourmet</a>
+        </div>     
+        <div class="col-sm-2">
+            <a href="#onsen">&#9658;&nbsp;&nbsp;Kitsuki <span class="floating-animate">O</span>n<span class="floating-animate">s</span>en</a>
+        </div>     
+    </div>
 </nav>
